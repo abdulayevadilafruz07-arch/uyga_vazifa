279a771756987d942b1a3f492da1c9e5e08b3de1
dilafruz <dilafruz@gmail.com>
2025-11-27 12:38:41 +0300

    vazifa da

--- vazifa.py
+++ vazifa.py
@@ -1,189 +1,128 @@
-# # contact manager
-# class Contact:
-#     def __init__(self, name, phone, email):
-#         self.name = name
-#         self.phone = phone
-#         self.email = email
-#
-# #
-# # contact1=Contact("dilafruz","+998900461045","dilafruz@gmail.com")
-# # baza=[contact1]
-# # # print(baza)
-#
-# class Sms:
-#     def __init__(self, to_phone,text):
-#         self.to_phone = to_phone
-#         self.text = text
-#
-#
-#
-# def view_contact(s:list):
-#     for item,c in enumerate(s):
-#         print(f"{item}. {c.name}, {c.phone}, {c.email}")
-#
-#
-# def add_contact(s:list):
-#     n_name=r'^[a-z0-9_-]{3,15}$'
-#     for i in range(3):
-#         name=input("ismi=")
-#         if re.match(n_name,name):
-#             break
-#         else:
-#             print("x")
-#     else:
-#         name=None
-#
-#     d_phone=r'^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$'
-#     for i in range(3):
-#         phone=input("phone=")
-#         if re.match(d_phone,phone):
-#             break
-#         else:
-#             print("x")
-#     else:
-#         phone=None
-#
-#     c_email=r'[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+'
-#     for i in range(3):
-#         email=input("email=")
-#         if re.match(c_email,email):
-#             break
-#         else:
-#             print("x")
-#     else:
-#         email=None
-#
-#     if name and phone and email:
-#         contact2=Contact(name,phone,email)
-#         s.append(contact2)
-#         print("qoshildi")
-#     else:
-#         print("qoshilmadi")
-#
-# import re
-# n=r'^[a-z0-9_-]{3,15}$'
-# d=r'^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$'
-# c=r'[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+'
-#
-#
-# def update_contact(s:list):
-#     view_contact(s)
-#     idx=input("yangilamaoqchi bolgan contact indeksini kiriting")
-#     if not idx.isdigit() or int(idx)>=len(s):
-#         print("bunday indeks yoq")
-#         return
-#     idx=int(idx)
-#     contact=s[idx]
-#     newname=input("yangi ism")
-#     if newname:
-#         if re.match(n,newname):
-#             contact.name=newname
-#         else:
-#             print("notogri ism")
-#
-#     newphone=input("raqami")
-#     if newphone:
-#         if re.match(d,newphone):
-#             contact.phone=newphone
-#         else:
-#             print("notogri phone")
-#
-#     newemail=input("emaili")
-#     if newemail:
-#         if re.match(c,newemail):
-#             contact.email=newemail
-#         else:
-#             print("notogri email")
-#     print("contact yangilandi")
-#
-#
-# def delete_contact(s:list):
-#     view_contact(s)
-#     idx=input("ochirmoqchi bolgan contact indeksi kiriting")
-#     if not idx.isdigit() or int(idx)>=len(s):
-#         print("bunday indeks yoq")
-#         return
-#     idx=int(idx)
-#     s.pop(idx)
-#     print("contact ochdi")
-#
-# def contact_manager(s:list):
-#     while True:
-#         kod=input("1. view contact\n2. add contact\n3.update contact\n4. delete contact\n5. exit")
-#         if kod=="1":
-#             view_contact(s)
-#         elif kod=="2":
-#             add_contact(s)
-#         elif kod=="3":
-#             update_contact(s)
-#         elif kod=="4":
-#             delete_contact(s)
-#         else:
-#             break
-# # contact_manager(baza)
-#
-# '''sms manager'''
-#
-# def sms_view(smsl:list):
-#     if not smsl:
-#         print(" sms yoq")
-#         return
-#     for i, s in enumerate(smsl):
-#         print(f"{i}. {s.to_phone}: {s.text}")
-#
-# def sms_send(s:list,smsl:list):
-#     if not s:
-#         print("contact yoq")
-#         return
-#
-#     view_contact(s)
-#     idx=input("qaysi contactga sms? (index): ")
-#     if not idx.isdigit() or int(idx)>=len(s):
-#         print("bunday indeks yoq")
-#         return
-#     idx=int(idx)
-#     text=input("sms matni")
-#
-#
-#     smsl.append(Sms(s[idx].phone,text))
-#     print("yuborildi")
-#
-# def sms_delete(smsl:list):
-#     sms_view(smsl)
-#     idx=input("qaysi sms ochiriladi")
-#     if not idx.isdigit() or int(idx)>=len(smsl):
-#         print("bunday indeks yoq")
-#         return
-#     smsl.pop(int(idx))
-#     print("sms ochdi")
-#
-# def sms_manager(s:list,smsl:list):
-#     if not s:
-#         print("contact yoq")
-#         return
-#     while True:
-#         kod=input("1. view sms\n2. send sms\n3. delete sms\n4. exit")
-#         if kod=="1":
-#             sms_view(smsl)
-#         elif kod=="2":
-#             sms_send(s,smsl)
-#         elif kod=="3":
-#             sms_delete(smsl)
-#         else:
-#             break
-#
-#
-# def manager_s_c():
-#     s=[]
-#     smsl=[]
-#     while True:
-#         kod=input("1.sms manager\n2.contact manager\n3. exit")
-#         if kod=="1":
-#             sms_manager(s,smsl)
-#         elif kod=="2":
-#             contact_manager(s)
-#         else:
-#             break
-#
-#
-# manager_s_c()
+
+'''Rent car'''
+'''1.class Car:
+2.class User:
+3.class RentCompany:'''
+
+class Car:
+    def __init__(self,name,year,price_per_day):
+        self.name = name
+        self.year = year
+        self.price_per_day = price_per_day
+        self.is_rented = False
+
+    def add_car(self):
+        name=input("name==")
+        year=input("year==")
+        price_per_day=input("price_per_day==")
+        car=Car(name,year,price_per_day)
+        self.cars.append(car)
+
+    def show_all(self):
+        count=0
+        for item in self.cars:
+            count+=1
+            print(f"{count}.name:{item.name},year:{item.year},price_per_day:{item.price_per_day}")
+
+    def update_car(self,index):
+        if 0<=index<len(self.cars):
+            car=self.cars[index]
+            new_name=input(f'Name({car.name}):')
+            new_year=input(f'Year({car.year}):')
+            new_price_per_day=input(f'Price({car.price_per_day}):')
+            car.name=new_name
+            car.year=new_year
+            car.price_per_day=new_price_per_day
+            print('malumot yangilandi')
+        else:
+            print('bunday index yoq')
+
+
+class User:
+    def __init__(self,first_name,last_name,salary):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+        self.rented_cars = []
+
+    def user_rented_cars(self):
+        count=0
+        for item in self.rented_cars:
+            count+=1
+            print(f'{count}.first_name:{item.first_name},last_name:{item.last_name},salary:{item.salary},rented_cars:{item.rented_cars}')
+
+
+class RentCompany:
+    def __init__(self):
+        self.balance = 0
+        self.cars = []
+        self.rented_cars = []
+
+
+# '''ijaraga berish'''
+    def rent_car(self,user,car_name):
+        car=next((c for c in self.cars if c.name.lower()==car_name.lower()), None)
+        if not car:
+            print('car topilmadi')
+            return
+        if any(car==c for c, u in self.rented_cars):
+            print('rented car. allaqachon ijarada')
+            return
+        if user.salary < car.price_per_day:
+            print('yetarli emas')
+            return
+
+        self.rented_cars.append((car,user))
+        user.rented_cars.append(car)
+        self.balance += car.price_per_day
+        print(f'{car} {user} ga ijaraga berildi.Balance: {self.balance}')
+
+# '''mashinani qaytarish'''
+    def return_car(self,user,car_name):
+        car= next((c for c in user.rented_cars if c.name.lower()==car_name.lower()), None)
+        if not car:
+            print('user bu car ni ijaraga olmagan')
+            return
+
+        self.rented_cars.remove((car,user))
+        user.rented_cars.remove(car)
+        print(f'{car} {user} tomonidan qaytarildi')
+
+
+
+def car_manager(Car):
+    while True:
+        kod=input("1.add car\n2.show all\n3.update car\n4.exit")
+        print('1.add car'
+              '2.show all'
+              '3.update car'
+              '4.exit')
+        if kod=='1':
+            Car.add_car()
+        elif kod=='2':
+            Car.show_all()
+        elif kod=='3':
+            Car.update_car(0)
+        else:
+            break
+
+car_manager(Car)
+
+def manager_r_u(company,users):
+    while True:
+        kod=input("1. rent car\n2.return car\n3.exit")
+        if kod=='1':
+            user_name=input("user name")
+            user=next((u for u in users if u.first_name.lower()==user_name.lower()), None)
+            if not user:
+                print('user topilmadi')
+                continue
+            car_name=input("car name")
+            company.return_car(user,car_name)
+        else:
+            break
+
+users=[User("ali","valiyev",400),User("bobur","karimov",600)]
+
+company=RentCompany()
+manager_r_u(company,users)
--- vazifa2.py
+++ vazifa2.py
@@ -165,3 +165,192 @@
 # erp_manager(erp)
 
 
+# # contact manager
+# class Contact:
+#     def __init__(self, name, phone, email):
+#         self.name = name
+#         self.phone = phone
+#         self.email = email
+#
+# #
+# # contact1=Contact("dilafruz","+998900461045","dilafruz@gmail.com")
+# # baza=[contact1]
+# # # print(baza)
+#
+# class Sms:
+#     def __init__(self, to_phone,text):
+#         self.to_phone = to_phone
+#         self.text = text
+#
+#
+#
+# def view_contact(s:list):
+#     for item,c in enumerate(s):
+#         print(f"{item}. {c.name}, {c.phone}, {c.email}")
+#
+#
+# def add_contact(s:list):
+#     n_name=r'^[a-z0-9_-]{3,15}$'
+#     for i in range(3):
+#         name=input("ismi=")
+#         if re.match(n_name,name):
+#             break
+#         else:
+#             print("x")
+#     else:
+#         name=None
+#
+#     d_phone=r'^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$'
+#     for i in range(3):
+#         phone=input("phone=")
+#         if re.match(d_phone,phone):
+#             break
+#         else:
+#             print("x")
+#     else:
+#         phone=None
+#
+#     c_email=r'[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+'
+#     for i in range(3):
+#         email=input("email=")
+#         if re.match(c_email,email):
+#             break
+#         else:
+#             print("x")
+#     else:
+#         email=None
+#
+#     if name and phone and email:
+#         contact2=Contact(name,phone,email)
+#         s.append(contact2)
+#         print("qoshildi")
+#     else:
+#         print("qoshilmadi")
+#
+# import re
+# n=r'^[a-z0-9_-]{3,15}$'
+# d=r'^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$'
+# c=r'[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+'
+#
+#
+# def update_contact(s:list):
+#     view_contact(s)
+#     idx=input("yangilamaoqchi bolgan contact indeksini kiriting")
+#     if not idx.isdigit() or int(idx)>=len(s):
+#         print("bunday indeks yoq")
+#         return
+#     idx=int(idx)
+#     contact=s[idx]
+#     newname=input("yangi ism")
+#     if newname:
+#         if re.match(n,newname):
+#             contact.name=newname
+#         else:
+#             print("notogri ism")
+#
+#     newphone=input("raqami")
+#     if newphone:
+#         if re.match(d,newphone):
+#             contact.phone=newphone
+#         else:
+#             print("notogri phone")
+#
+#     newemail=input("emaili")
+#     if newemail:
+#         if re.match(c,newemail):
+#             contact.email=newemail
+#         else:
+#             print("notogri email")
+#     print("contact yangilandi")
+#
+#
+# def delete_contact(s:list):
+#     view_contact(s)
+#     idx=input("ochirmoqchi bolgan contact indeksi kiriting")
+#     if not idx.isdigit() or int(idx)>=len(s):
+#         print("bunday indeks yoq")
+#         return
+#     idx=int(idx)
+#     s.pop(idx)
+#     print("contact ochdi")
+#
+# def contact_manager(s:list):
+#     while True:
+#         kod=input("1. view contact\n2. add contact\n3.update contact\n4. delete contact\n5. exit")
+#         if kod=="1":
+#             view_contact(s)
+#         elif kod=="2":
+#             add_contact(s)
+#         elif kod=="3":
+#             update_contact(s)
+#         elif kod=="4":
+#             delete_contact(s)
+#         else:
+#             break
+# # contact_manager(baza)
+#
+# '''sms manager'''
+#
+# def sms_view(smsl:list):
+#     if not smsl:
+#         print(" sms yoq")
+#         return
+#     for i, s in enumerate(smsl):
+#         print(f"{i}. {s.to_phone}: {s.text}")
+#
+# def sms_send(s:list,smsl:list):
+#     if not s:
+#         print("contact yoq")
+#         return
+#
+#     view_contact(s)
+#     idx=input("qaysi contactga sms? (index): ")
+#     if not idx.isdigit() or int(idx)>=len(s):
+#         print("bunday indeks yoq")
+#         return
+#     idx=int(idx)
+#     text=input("sms matni")
+#
+#
+#     smsl.append(Sms(s[idx].phone,text))
+#     print("yuborildi")
+#
+# def sms_delete(smsl:list):
+#     sms_view(smsl)
+#     idx=input("qaysi sms ochiriladi")
+#     if not idx.isdigit() or int(idx)>=len(smsl):
+#         print("bunday indeks yoq")
+#         return
+#     smsl.pop(int(idx))
+#     print("sms ochdi")
+#
+# def sms_manager(s:list,smsl:list):
+#     if not s:
+#         print("contact yoq")
+#         return
+#     while True:
+#         kod=input("1. view sms\n2. send sms\n3. delete sms\n4. exit")
+#         if kod=="1":
+#             sms_view(smsl)
+#         elif kod=="2":
+#             sms_send(s,smsl)
+#         elif kod=="3":
+#             sms_delete(smsl)
+#         else:
+#             break
+#
+#
+# def manager_s_c():
+#     s=[]
+#     smsl=[]
+#     while True:
+#         kod=input("1.sms manager\n2.contact manager\n3. exit")
+#         if kod=="1":
+#             sms_manager(s,smsl)
+#         elif kod=="2":
+#             contact_manager(s)
+#         else:
+#             break
+#
+#
+# manager_s_c()
